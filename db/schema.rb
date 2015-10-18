05fe8bbe2aa5e5c01d99b7a07ccaa50ab74d887b
Steven Cassidy <steven.cassidy@gmail.com>
2015-10-18 18:02:06 -0400

    Generate base models

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018215540) do
+ActiveRecord::Schema.define(version: 20151018220150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "display_order"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "topic_id"
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "conversations", ["topic_id"], name: "index_conversations_on_topic_id", using: :btree
+  add_index "conversations", ["user_id"], name: "index_conversations_on_user_id", using: :btree
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
+
+  create_table "topics", force: :cascade do |t|
+    t.integer  "category_id"
+    t.string   "name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "topics", ["category_id"], name: "index_topics_on_category_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -24,4 +62,9 @@ ActiveRecord::Schema.define(version: 20151018215540) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "conversations", "topics"
+  add_foreign_key "conversations", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
+  add_foreign_key "topics", "categories"
 end
