fa799cea3ed58088255dd1cb5e3b2232502a6612
Steven Cassidy <steven.cassidy@gmail.com>
2015-10-18 18:07:16 -0400

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+u = User.create!(username:'admin', email:Faker::Internet.email, password:'pw123456')
+Category.create!(name:'General', display_order:10)
+Category.create!(name:'Tech advice', display_order:20)
+Category.create!(name:'Product guides', display_order:30)
+Category.create!(name:'Miscellaneous', display_order:40)
+cats = Category.all
+
+12.times do
+  top = Topic.create!(name: Faker::Company.catch_phrase, category: cats.sample)
+  6.times do
+    thr = Conversation.create!(name:Faker::Commerce.product_name, topic:top, author:u )
+    Random.rand(20).times do
+      msg = Message.create!(user:u, conversation:thr, content:Faker::Lorem.sentence)
+    end
+  end
+end
