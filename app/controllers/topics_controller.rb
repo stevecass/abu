83549b45b2dbff1a291ff153bcd59efea0d2d055
Steven Cassidy <steven@mumsnet.com>
2015-10-19 15:20:47 -0400

    Add topics#show route to redirect to conversations

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,4 +2,9 @@ class TopicsController < ApplicationController
   def index
     @categories = Category.includes(:topics).order(:display_order)
   end
+
+  def show
+    @topic = Topic.find(params[:id])
+    redirect_to topic_conversations_path(@topic)
+  end
 end
