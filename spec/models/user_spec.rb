242be6621b2a33cebbbc8f50c4cdbd0976f2305b
Steven Cassidy <steven@mumsnet.com>
2015-10-22 17:43:25 -0400

    Fix minor bug

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User do
   it { should validate_presence_of :email }
 
   it 'should respond to email' do
-    expect(user).not_to respond_to(:email)
+    expect(user).to respond_to(:email)
   end
 
 end
