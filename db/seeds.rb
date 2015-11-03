c2d885242261ee729626900d9fe5ce382741e571
Steven Cassidy <steven@mumsnet.com>
2015-11-03 14:41:05 -0500

    Improve seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,28 @@
 
 
 u = User.create!(username:'admin', email:Faker::Internet.email, password:'pw123456')
+30.times { User.create!(username: Faker::Internet.user_name, email:Faker::Internet.email, password:'pw123456') }
+puts "Created users"
 Category.create!(name:'General', display_order:10)
 Category.create!(name:'Tech advice', display_order:20)
 Category.create!(name:'Product guides', display_order:30)
 Category.create!(name:'Miscellaneous', display_order:40)
 cats = Category.all
+users = User.all
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+puts "Created categories"
 
 12.times do
+  print "."
   top = Topic.create!(name: "#{Faker::App.name} #{alphabet.sample} #{Faker::App.name}", category: cats.sample)
-  6.times do
-    thr = Conversation.create!(name:Faker::Commerce.product_name, topic:top, author:u )
-    Random.rand(20).times do
-      msg = Message.create!(user:u, conversation:thr, content:Faker::Lorem.paragraphs(3).join("\n"))
+  16.times do
+    print "+"
+    thr = Conversation.create!(name:Faker::Commerce.product_name, topic:top, author:users.sample )
+    Random.rand(120).times do
+      msg = Message.create!(user:users.sample, conversation:thr, content:Faker::Lorem.paragraphs(3).join("\n"))
     end
   end
+  puts
+
 end
